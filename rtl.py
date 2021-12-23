057a3281f8959fe1702419e09c6e805dc514bca8
Shay <shaymintzer@gmail.com>
2021-12-23 10:43:05 +0200

    HEBREW SEARCH

--- rtl.py
+++ rtl.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import os
 replacement = '<html dir="rtl" lang="" xml:lang="">}'
 for dname, dirs, files in os.walk(r"C:\R\R"):
@@ -10,3 +12,18 @@ for dname, dirs, files in os.walk(r"C:\R\R"):
             s = s.replace('<html lang="" xml:lang="">', replacement)
             with open(fpath, "w") as f:
                 f.write(s)
+
+replacement2 = 'מה תרצי לחפש? (Enter כדי לנווט)'
+replacement3 = 'הקלד כדי לחפש'
+for dname, dirs, files in os.walk(r"C:\R\R"):
+    for fname in files:
+        if fname[-3:] == '.js':
+            print(fname)
+            fpath = os.path.join(dname, fname)
+            with open(fpath) as f:
+                s = f.read()
+            s = s.replace('Type to search (Enter for navigation)', replacement2)
+            s = s.replace('Type to search', replacement3)
+            with open(fpath, "w") as f:
+                f.write(s)
+
